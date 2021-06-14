1bc1b2cc1eb75041a4dd4bec950193ade7ef61a7
Crew Spence <csspence123@gmail.com>
2021-06-14 06:57:01 -0500

    Created function stub for toy problem.

---
+++ codewars_find_the_squares.js
@@ -0,0 +1,17 @@
+/*
+You are to write a function that takes an odd integer which is the difference between two consecutive perfect squares and find the squares
+
+Input Format
+num : Odd Integer 0<n<1000000
+
+Output Format
+Answer: String containing the bigger and smaller squares in one line like "Max-Min"
+
+Examples
+findSquares(9)  //returns '25-16'
+findSquares(5)  // returns '9-4' 
+*/
+
+const findSquares = num => {
+//     return `${max}-${min}`;
+};
